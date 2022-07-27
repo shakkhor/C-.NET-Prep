d74d1f6823d6b686afc65a878d5ebe97a26d51e0
Abdullah Aziz Sharfuddin Shakkhor <abdullahshakkhor@gmail.com>
2022-07-27 16:07:53 +0600

    up to exception complete.

---
+++ src/TypesOfErrors/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        //compilation error - missing semicolon
+        //var number = 5
+
+        //the code below will execute without exception, but the result will not be as expected
+        //expected result is A little duck swims in a pond
+        var sentence = MergeWords("A", "little", "duck", "swims", "in", "a", "pond");
+        Console.WriteLine(sentence);
+
+        //the code below will produce a runtime error because we try to access first element of an empty list
+        var list = new List<int>();
+        var firstElement = list.First();
+
+        Console.ReadKey();
+    }
+
+    private static object MergeWords(params string[] words)
+    {
+        return string.Join("", words);
+        //should be:
+        //return string.Join(" ", words);
+    }
+}
